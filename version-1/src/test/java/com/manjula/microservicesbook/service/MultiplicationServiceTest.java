d87ec341622aa559328e36f0b018ff5b14f9ca82
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2017-12-31 12:48:12 +0530

    upgrade version 1 to junit 5

--- version-1/src/test/java/com/manjula/microservicesbook/service/MultiplicationServiceTest.java
+++ version-1/src/test/java/com/manjula/microservicesbook/service/MultiplicationServiceTest.java
@@ -1,17 +1,17 @@
 package com.manjula.microservicesbook.service;
 
 import com.manjula.microservicesbook.domain.Multiplication;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class MultiplicationServiceTest {
 
